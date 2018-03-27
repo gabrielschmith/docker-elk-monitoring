a6449115183358e6c7773d5e343e420692fca2be
gabrielschmith <gabrielschmith@hotmail.com>
2018-03-27 15:33:06 -0300

    Refactor curator dockerfile

---
+++ curator/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+# Add curator as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- curator "$@"
+fi
+
+# Step down via gosu  
+if [ "$1" = 'curator' ]; then
+	exec gosu curator bash -c "while true; do curator --config $PATH_CONFIG $PATH_ACTIONS; set -e; sleep $(( 60*60*INTERVAL_IN_HOURS )); set +e; done"
+fi
+
+# As argument is not related to curator,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
